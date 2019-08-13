5ab2e72b262774699bb5e5421cb7df2a75aa3cec
carlostaffe <carlos.taffe@gmail.com>
2019-08-13 15:21:56 +0000

    primera clase sockets ... estudien la teoria de tanenbaum, por ejemplo ;-)

---
+++ network/server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import socket
+
+desc= socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
+servidor=raw_input("mi ip:")
+port=raw_input("mi port:")
+desc.bind((servidor, int(port)))
+desc.listen(1)
+newdesc,cli = desc.accept()
+print cli
+leido = newdesc.recv(2048)
+print leido
+newdesc.send("saludos cliente\n")
+newdesc.close()
