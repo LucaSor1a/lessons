72ea0581411b9ccb8cfc7ae4030d472ec1b61f0b
carlostaffe <carlos.taffe@gmail.com>
2019-10-08 15:36:10 +0000

    paralelizacion usando pool, async y callback

--- paralelizacion/descompone-pool-apply_async.py
+++ paralelizacion/descompone-pool-apply_async.py
@@ -15,7 +15,7 @@ def call (retorno):
 #Completo el arreglo de mi lista
 lista = [1,2,3,4,8,12,4,5]
 lista2 = []
-#nro_hijos = len (lista)
+nro_elementos = len (lista)
 #pul = mp.Pool(mp.cpu_count())
 pool = mp.Pool(4) #crea 4 hijos trabajadores
 sub = 0
@@ -23,7 +23,10 @@ for elemento in lista:
     pool.apply_async(cuadrado, args=(sub,elemento,), callback=call)
     sub = sub + 1
 pool.close()
-pool.join()
+pool.join() # para esperar que terminen los workers
 print ("terminaron los 4 hijos")
-print (lista)
-print (lista2)
+print (lista) 
+listaf = lista # para ahorrar el bucle con append
+for i in range(nro_elementos):
+    listaf[lista2[i][0]] = lista2[i][1]
+print (listaf)
