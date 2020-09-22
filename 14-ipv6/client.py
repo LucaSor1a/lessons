4958da09335e214ccf701f1847144303a939a11b
carlost <carlos.taffe@gmail.com>
2020-09-22 11:20:55 -0300

    cliente y server ipv4/ipv6

--- 14-ipv6/client.py
+++ 14-ipv6/client.py
@@ -1,10 +1,14 @@
 import socket
+servidor = "www.google.com"
+direcciones = socket.getaddrinfo(servidor, 80,  0 , 1 )
 
-s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-
-s.connect(("www.python.org", 80))
-s.send(b"GET / HTTP/1.1\r\nHost:www.python.org\r\n\r\n")
-response = s.recv(1024)
-print (response)
-s.close()
+for direccion in direcciones:
+    s = socket.socket(direccion[0], direccion[1])
+#    s = socket.socket(direcciones[0], socket.SOCK_STREAM)
+    print ("me conecto a:", direccion[4] )
+    s.connect((direccion[4]))
+    s.send(bytearray("GET / HTTP/1.1\r\nHost:" + servidor + "\r\n\r\n","utf-8"))
+    response = s.recv(1024)
+    print (response)
+    s.close()
 
